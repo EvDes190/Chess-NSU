f180d0ab7dd2941c47d86df2627bc0d19d68b641
EvDes190 <89368408+EvDes190@users.noreply.github.com>
2026-04-12 18:12:42 +0300

    cdm16 finally work

--- program.asm
+++ program.asm
@@ -1,6 +1,25 @@
 asect 0
-inc fp
-halt
-end
+main: ext
+default_handler: ext
+
+
+dc main, 0
+dc default_handler, 0
+dc default_handler, 0
+dc default_handler, 0
+dc default_handler, 0
+align 0x80
+
+
+rsect exc_handlers
+
+default_handler>
+    halt
+rsect main
+
+main>
+    
+    halt
+end.
 
 
